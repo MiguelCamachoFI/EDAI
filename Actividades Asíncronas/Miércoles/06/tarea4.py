41638ccdf25c8f642bdcf4a17ffb2ebbc3bef9f5
Miguel Angel Camacho Garduño <41399255+Mike-Camacho@users.noreply.github.com>
2021-08-05 11:52:13 -0500

    Update tarea4.py

--- Actividades Asíncronas/Miércoles/06/tarea4.py
+++ Actividades Asíncronas/Miércoles/06/tarea4.py
@@ -9,8 +9,30 @@ def binaryToDecimal(binary):
         binary = binary//10
         i += 1
     #Imprimir el valor en decimales
-    print(decimal)   
+    print("El valor en decimal de",bynary, "es",decimal)   
 #Código de inicio
 if __name__ == '__main__':
-    bynary=int(input("Ingrese un valor binario de 4 bits a convertir: "))
+    #Evaluar si el número ingresado es un número binario
+    while True:
+        try:
+            # Pedirle al usuario que ingrese un número binario de 4 bits a convertir
+            bynary = int(input("Ingrese un número binario de 4 bits: "))
+        except ValueError: # Si el valor no es un número entero
+            print('Debe de ser un número binario (contener solo 1s y 0s)')
+        else:
+            # Iteractuar a través de todos los dígitos en x
+            for i in str(bynary):
+                if i in '10': # Si el dígito es 1 o 0
+                    binary = True
+                else:
+                    binary = False
+                    break
+            if binary == False:
+                print('Debe ser un número binario (contener solo 1s y 0s)')
+            elif len(str(bynary))!=4:
+                print("Debe ser un número de 4 bits")
+            else:
+                break # El número es binario, es seguro romper con el bucle.
+    #Imprimir número binario ingresado
+    print(bynary, "es binario y de 4 dígitos")
     binaryToDecimal(bynary)
